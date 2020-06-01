b550673df13e3b2cfbab5f474a84ea4a5077bdd5
RadikPolt <radikpolt@gmail.com>
2020-06-01 06:30:19 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let div = document.querySelector('.ba-textarea');
+let textA = document.querySelector('#textarea');
+
+document.addEventListener('keydown', editorText)
+
+function editorText(event) {
+// Ctrl + E
+	if ( event.ctrlKey && event.code == 'KeyE') { 
+		event.preventDefault();
+		textA.value = div.innerHTML;
+		div.hidden = true;
+		textA.hidden = false;
+		textA.focus();
+	}
+// Ctrl +S
+	if (event.ctrlKey && event.code == 'KeyS') { 
+		event.preventDefault();
+		div.innerHTML = textA.value
+		div.hidden = false;
+		textA.hidden = true;
+	}
+// Esc
+	if (event.code == 'Escape') { 
+		event.preventDefault();
+		div.hidden = false;
+		textA.hidden = true; 
+	}
+}
